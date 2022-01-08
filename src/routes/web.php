28818efc35801346ff6f35cbdc24a4149ff78649
reza309 <reza112sh@gmail.com>
2022-01-08 20:34:38 +0600

    release version v2.0.9

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,8 +3,9 @@
 use App\Http\Controllers\Auth\ItCornerAuthController as AuthItCornerAuthController;
 use Illuminate\Support\Facades\Route;
 use ItCorner\Auth\Http\Controllers\Auth\ItCornerAuthController;
-Route::get('login',[ItCornerAuthController::class,"login"])->name('login');
+
 Route::group(['middleware' => ['web']], function () {
+    Route::get('login',[ItCornerAuthController::class,"login"])->name('login');
     Route::get('register',[ItCornerAuthController::class,"registerView"])->name('register');
     Route::post('register',[ItCornerAuthController::class,'register'])->name('register');
 });
