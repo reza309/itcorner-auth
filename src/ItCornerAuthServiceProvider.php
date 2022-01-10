b220e06db94d4e5dff09a57b57fc0ee988bb3e97
reza309 <reza112sh@gmail.com>
2022-01-10 18:55:10 +0600

    release v2.0.20

--- src/ItCornerAuthServiceProvider.php
+++ src/ItCornerAuthServiceProvider.php
@@ -36,7 +36,7 @@ class ItCornerAuthServiceProvider extends ServiceProvider
             __DIR__.'/routes'=>"routes",
             __DIR__.'/public' => public_path(),
             __DIR__.'/migrations' => database_path('migrations/'),
-            __DIR__.'/Http/kernel.php' => app_path('Http/kernel.php'),
+            // __DIR__.'/Http/kernel.php' => app_path('Http/kernel.php'),
         ],'it-corner-auth-publish');
 
 
